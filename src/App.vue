22546de62bd537811cdaa60e066cf814e889bcc4
Pedro Soares <pedro.soares@camptocamp.com>
2024-12-05 21:49:08 +0100

    remove unused css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
 import './assets/main.css'
 </script>
 
@@ -7,29 +7,3 @@ import './assets/main.css'
   <RouterView />
 </template>
 
-<style scoped>
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-</style>
